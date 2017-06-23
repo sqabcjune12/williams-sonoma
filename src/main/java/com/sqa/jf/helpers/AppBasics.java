40523e7ef58d0ec041645d95a5979dc37c3644d6
Jean-francois Nepton <sqasolution@hotmail.com>
2017-06-22 18:43:55 -0700

    Update AppBasics with requestInput Methods

--- src/main/java/com/sqa/jf/helpers/AppBasics.java
+++ src/main/java/com/sqa/jf/helpers/AppBasics.java
@@ -2,6 +2,8 @@ package com.sqa.jf.helpers;
 
 import java.util.*;
 
+import com.sqa.jf.helpers.exceptions.*;
+
 public class AppBasics {
 
 	private static Scanner scanner = new Scanner(System.in);
@@ -22,5 +24,149 @@ public class AppBasics {
 		System.out.println("Welcome to the " + appName + " Application.");
 		System.out.print("Could I get your name please: ");
 		return scanner.nextLine();
+	}
+
+	public static boolean requestBoolean(String question) {
+		boolean isInvalid = true;
+		boolean response = false;
+		String input = "";
+		while (isInvalid) {
+			System.out.print(question + " (Yes/No): ");
+			input = scanner.nextLine();
+			try {
+				if (input.equalsIgnoreCase("yes")) {
+					isInvalid = false;
+					response = true;
+				} else if (input.equalsIgnoreCase("no")) {
+					isInvalid = false;
+					response = false;
+				} else {
+					throw new InvalidBooleanResponse();
+				}
+			} catch (InvalidBooleanResponse e) {
+				System.out.println("You have an incorrect response (Yes/No) [" + input + "]");
+			}
+		}
+		return response;
+	}
+
+	public static int requestByte(String question) {
+		byte value = 0;
+		boolean isInvalid = true;
+		while (isInvalid) {
+			System.out.print(question + " ");
+			String input = scanner.nextLine();
+			try {
+				value = Byte.parseByte(input.trim());
+				isInvalid = false;
+			} catch (NumberFormatException e) {
+				System.out.println("You have not entered a correct formatted number [" + input + "]");
+			}
+		}
+		return value;
+	}
+
+	public static char requestChar(String question) {
+		boolean isInvalid = true;
+		String input = "";
+		while (isInvalid) {
+			System.out.print(question + " ");
+			input = scanner.nextLine();
+			try {
+				if (input.length() > 1) {
+					throw new InvalidCharResponseLength();
+				}
+				isInvalid = false;
+			} catch (InvalidCharResponseLength e) {
+				System.out.println("You have entered too many characters [" + input + "]");
+			}
+		}
+		return input.charAt(0);
+	}
+
+	public static double requestDouble(String question) {
+		double value = 0;
+		boolean isInvalid = true;
+		while (isInvalid) {
+			System.out.print(question + " ");
+			String input = scanner.nextLine();
+			try {
+				value = Double.parseDouble(input.trim());
+				isInvalid = false;
+			} catch (NumberFormatException e) {
+				System.out.println("You have not entered a correct decimal formatted number [" + input + "]");
+			}
+		}
+		return value;
+	}
+
+	public static float requestFloat(String question) {
+		float value = 0;
+		boolean isInvalid = true;
+		while (isInvalid) {
+			System.out.print(question + " ");
+			String input = scanner.nextLine();
+			try {
+				value = Float.parseFloat(input.trim());
+				isInvalid = false;
+			} catch (NumberFormatException e) {
+				System.out.println("You have not entered a correct decimal formatted number [" + input + "]");
+			}
+		}
+		return value;
+	}
+
+	public static int requestInt(String question) {
+		int value = 0;
+		boolean isInvalid = true;
+		while (isInvalid) {
+			System.out.print(question + " ");
+			String input = scanner.nextLine();
+			try {
+				value = Integer.parseInt(input.trim());
+				isInvalid = false;
+			} catch (NumberFormatException e) {
+				System.out.println("You have not entered a correct formatted number [" + input + "]");
+			}
+		}
+		return value;
+	}
+
+	public static long requestLong(String question) {
+		long value = 0;
+		boolean isInvalid = true;
+		while (isInvalid) {
+			System.out.print(question + " ");
+			String input = scanner.nextLine();
+			try {
+				value = Long.parseLong(input.trim());
+				isInvalid = false;
+			} catch (NumberFormatException e) {
+				System.out.println("You have not entered a correct formatted number [" + input + "]");
+			}
+		}
+		return value;
+	}
+
+	public static short requestShort(String question) {
+		short value = 0;
+		boolean isInvalid = true;
+		while (isInvalid) {
+			System.out.print(question + " ");
+			String input = scanner.nextLine();
+			try {
+				value = Short.parseShort(input.trim());
+				isInvalid = false;
+			} catch (NumberFormatException e) {
+				System.out.println("You have not entered a correct formatted number [" + input + "]");
+			}
+		}
+		return value;
+	}
+
+	public static String requestString(String question) {
+		System.out.print(question + " ");
+		String input = scanner.nextLine();
+		return input.trim();
 	}
 }
---
+++ src/main/java/com/sqa/jf/helpers/exceptions/InvalidCharResponseLength.java
@@ -0,0 +1,27 @@
+/**
+ *   File Name: InvalidCharResponseLength.java<br>
+ *
+ *   Nepton, Jean-francois<br>
+ *   Java Boot Camp Exercise<br>
+ *   Instructor: Jean-francois Nepton<br>
+ *   Created: Jun 21, 2017
+ *   
+ */
+
+package com.sqa.jf.helpers.exceptions;
+
+
+/**
+ * InvalidCharResponseLength //ADDD (description of class)
+ * <p>
+ * //ADDD (description of core fields)
+ * <p>
+ * //ADDD (description of core methods)
+ * 
+ * @author      Nepton, Jean-francois
+ * @version     1.0.0
+ * @since       1.0
+ *
+ */
+public class InvalidCharResponseLength extends Exception {
+}
